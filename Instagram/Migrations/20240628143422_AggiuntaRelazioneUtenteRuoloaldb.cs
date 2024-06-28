1ff28ef1d9611ace70dbbc6f6af058c38e8f2bc2
paolo flore <paoloflore937@gmail.com>
2024-06-28 16:37:02 +0200

    tabelle ruoli e tabella ponte ruolo utente create

---
+++ Instagram/Migrations/20240628143422_AggiuntaRelazioneUtenteRuoloaldb.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Instagram.Migrations
+{
+    public partial class AggiuntaRelazioneUtenteRuoloaldb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ruolo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UtenteRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UtenteId = table.Column<int>(type: "int", nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    RolesId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UtenteRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UtenteRoles_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UtenteRoles_Utente_UtenteId",
+                        column: x => x.UtenteId,
+                        principalTable: "Utente",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UtenteRoles_RolesId",
+                table: "UtenteRoles",
+                column: "RolesId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UtenteRoles_UtenteId",
+                table: "UtenteRoles",
+                column: "UtenteId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UtenteRoles");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
